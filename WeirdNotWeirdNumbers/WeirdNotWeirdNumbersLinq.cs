a836d8bd10f83fa5bda82fab1af78b875a6fccfd
Richard Oden <richard.thomas.oden@gmail.com>
2020-10-07 16:23:16 -0400

    Add solution to WeirdNotWeirdNumbers using LINQ

---
+++ WeirdNotWeirdNumbers/WeirdNotWeirdNumbersLinq.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace WeirdNotWeirdNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var possibleNumbers = Enumerable.Range(1, 100);
+            var weirdNumbers = possibleNumbers.Where(i => i%2 != 0 || i >= 6 && i <= 50);
+            var notWeirdNumbers = possibleNumbers.Concat(weirdNumbers).Distinct();
+            
+            int n = int.Parse(Console.ReadLine());
+            if (weirdNumbers.Any(w => w == n)) 
+            {
+                Console.WriteLine("Weird");
+            }
+            else if (notWeirdNumbers.Any(nW => nW == n))
+            {
+                Console.WriteLine("Not Weird");
+            }
+        }
+    }
+}
